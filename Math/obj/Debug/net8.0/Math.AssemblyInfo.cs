47848874011208e714266ac6d7119a0366a13fe1
Mehmet <mehmetatay0107@gmail.com>
2024-01-07 22:26:15 +0300

    Homeworks

--- Math/obj/Debug/net8.0/Math.AssemblyInfo.cs
+++ Math/obj/Debug/net8.0/Math.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+132c4c1143714838340a7c8af38cd0b25c990097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8bcb11f9c7fe46cdf79e27d14e90f9d653810f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
