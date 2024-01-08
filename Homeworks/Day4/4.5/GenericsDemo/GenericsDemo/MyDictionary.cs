3e3be8cb168fc16d1791b6233f3bc2678e73586c
Mehmet <mehmetatay0107@gmail.com>
2024-01-08 16:48:08 +0300

    Generic-Dictionary

--- Homeworks/Day4/4.5/GenericsDemo/GenericsDemo/MyDictionary.cs
+++ Homeworks/Day4/4.5/GenericsDemo/GenericsDemo/MyDictionary.cs
@@ -1,17 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Cryptography;
 
 namespace GenericsDemo
 {
-    internal class MyDictionary<T1,T2>
+    internal class MyDictionary<TKey, TValue>
     {
-        
-        public void Add(T1 item1, T2 item2) 
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+        public MyDictionary() 
         {
+            keys = new TKey[0] { };
+            values = new TValue[0] { };
+        }
+        public void Add(TKey item1, TValue item2) 
+        {
+            tempKeys = keys;
+            keys = new TKey[keys.Length + 1];
+            tempValues = values;
+            values = new TValue[values.Length + 1];
 
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = item1;
+            values[values.Length - 1] = item2;
+        }
+
+        public int Length()
+        {
+            return keys.Length;
+        }
+
+        public TKey GetKey(int index)
+        {
+            return keys[index];
+        }
+
+        
+        public TValue GetValue(TKey key)
+        {
+            keys[0] = key;
+            bool keyExists = false;
+            int index = 0;
+            for (int i = 0;i < keys.Length;i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    keyExists = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (keyExists == false) 
+            {
+                throw new Exception(key + " isimli bir anahtar bulunamadı.");
+            }
+            else
+            {
+                return values[index];
+            }
         }
     }
 }
--- Homeworks/Day4/4.5/GenericsDemo/GenericsDemo/Program.cs
+++ Homeworks/Day4/4.5/GenericsDemo/GenericsDemo/Program.cs
@@ -19,8 +19,14 @@
             }
 
             MyDictionary<int, string> footballers2 = new MyDictionary<int, string>();
+            int size = footballers2.Length();
+            Console.WriteLine(size);
+            footballers2.Add(17, "Emrah Atay");
+            int size2 = footballers2.Length();
+            Console.WriteLine(size2);
+            Console.WriteLine(footballers2.GetKey(0));
+            Console.WriteLine(footballers2.GetValue(17));
 
-            
 
         }
     }
