c56c83b296919eb1b44f3b2b6bba5d6d5d9e2c33
Mehmet <mehmetatay0107@gmail.com>
2024-01-27 19:52:41 +0300

    Homework-12.1

--- Homeworks/ReCapProject/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Homeworks/ReCapProject/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f0b7a6bd2a1032c2c1212a2fba3e6d954f6051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8251cbd327180eb3ca572f04f132c31d072080")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Homeworks/ReCapProject/ConsoleUI/obj/Debug/net8.0/ConsoleUI.AssemblyInfo.cs
+++ Homeworks/ReCapProject/ConsoleUI/obj/Debug/net8.0/ConsoleUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f0b7a6bd2a1032c2c1212a2fba3e6d954f6051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
